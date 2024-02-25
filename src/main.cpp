9ef95cba5f8e13f41a1aff0e112612fa53a35d9b
= <=>
2024-02-25 15:39:46 +1300

    added main file

---
+++ src/main.cpp
@@ -0,0 +1,51 @@
+/*
+.--------------------------------------------------------------------------.
+|                                                                          |
+|  iMate 		             chess engine                                  |
+|  Copyright (C) 2024        Martin Newbound                               |
+|                                                                          |
+|  for enquires, contact     martin.nz.dev@gmail.com                       |
+|                                                                          |
+|  This program is free software: you can redistribute it and/or modify    |
+|  it under the terms of the GNU General Public License as published by    |
+|  the Free Software Foundation, either version 3 of the License, or       |
+|  (at your option) any later version.                                     |
+|                                                                          |
+|  This program is distributed in the hope that it will be useful,         |
+|  but WITHOUT ANY WARRANTY; without even the implied warranty of          |
+|  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the           |
+|  GNU General Public License for more details.                            |
+|                                                                          |
+|  You should have received a copy of the GNU General Public License       |
+|  along with this program. If not, see https://www.gnu.org/licenses/      |
+|                                                                          |
+'--------------------------------------------------------------------------'
+*/
+
+#include <iostream>
+
+
+
+namespace iMate {
+
+	constexpr char STDOUT_COPYRIGHT_NOTICE[] =        "  \n   \
+		iMate,  Copyright  (C)  2024   Martin Newbound   \n   \
+		This program comes with ABSOLUTELY NO WARRANTY   \n   \
+		for enquires, contact martin.nz.mail@gmail.com   \n   ";    
+}
+
+
+int main(int argc, char *argv[])
+{
+	std::cout << iMate::STDOUT_COPYRIGHT_NOTICE << std::endl; 
+	
+
+	// init engine
+	
+	// start multi threading
+	
+	// ready
+
+}
+
+
